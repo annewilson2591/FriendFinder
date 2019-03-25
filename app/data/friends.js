c5dae7e5bfec2f7a183d2146eeeef591733f29f3
Anne Wilson <annewilson2591@gmail.com>
2019-03-24 21:21:39 -0600

    update

---
+++ app/data/friends.js
@@ -0,0 +1,36 @@
+var friends = [
+    {
+        name: "John",
+        photo: "https://via.placeholder.com/150",
+        scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+        ]
+    },
+    {
+        name: "Mike",
+        photo: "https://via.placeholder.com/150",
+        scores: [
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2",
+            "2"
+        ]
+    }
+];
+
+module.exports = friends;
