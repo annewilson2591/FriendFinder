c54385982d1c5e4b201071aa1764aded90d66501
Anne Wilson <annewilson2591@gmail.com>
2019-03-28 18:24:23 -0600

    resolved json

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,8 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
 
-    app.use(function(req, res) {
+    app.get("/", function(req, res) {
+        console.log("match!")
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
 }
